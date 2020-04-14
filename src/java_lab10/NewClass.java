c510c5fc91afd951df0351441a73f510657b06c5
ajulapraju <59993649+ajulapraju@users.noreply.github.com>
2020-04-14 16:26:41 +0530

    Java_Lab Program

---
+++ src/java_lab10/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_lab10;
+
+import java_lab.*;
+
+/**
+ *
+ * @author sajitha
+ */
+public class NewClass {
+    
+}
