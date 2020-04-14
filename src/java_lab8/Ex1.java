35717bc21e66be1ee81d036d8874ffab06f3a0e4
ajulapraju <59993649+ajulapraju@users.noreply.github.com>
2020-04-14 19:43:18 +0530

    Add files via upload

---
+++ src/java_lab8/Ex1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_lab8;
+
+/**
+ *
+ * @author P.P RAJU
+ */import P1.*;
+public class Ex1 {
+   public static void main(String args[])
+   {
+       Ex e=new Ex();
+       e.display();
+   }
+    
+}
